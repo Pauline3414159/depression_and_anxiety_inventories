f9bc736b947833c6ecfad1cfaedc49aa86b886ea
Pauline3414159 <fagie37@hotmail.com>
2021-09-06 13:57:42 -0400

    started to fill out some routs with placeholders

---
+++ d_burns_app.rb
@@ -0,0 +1,37 @@
+require 'sinatra'
+require 'tilt'
+require 'erubis'
+require 'sinatra/reloader'
+
+get "/" do
+  redirect "/home"
+  erb :home
+end
+
+get "/home" do
+  erb :home
+end
+
+get "/depression/new" do
+  [200, "\n\n\n\n nothing here yet"]
+end
+
+get "/anxiety/new" do
+  [200, "\n\n\n\n nothing here yet"]
+end
+
+post "anxiety/new" do
+  "nothing here yet"
+end
+
+post "depression/new" do
+  
+end
+
+get "/depression/reports" do
+  
+end
+
+get "depressions/reports" do
+  
+end
